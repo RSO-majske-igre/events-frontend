db7bba5c7938074c39603b56734484c6eef8f293
bizjak3 <36822599+bizjak3@users.noreply.github.com>
2023-01-11 22:37:53 +0100

    Update environment.prod.ts
    removed /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  eventsBaseUrl: "http://34.168.23.255:8090/",
-  participantsBaseUrl: "http://34.168.57.248:8110/"
+  eventsBaseUrl: "http://34.168.23.255:8090",
+  participantsBaseUrl: "http://34.168.57.248:8110"
 };
